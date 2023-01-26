0d0e9a6539308ebf17486fc30d8c8ef3d5aa0ed4
Tpho <thipho.doan@gmail.com>
2023-01-26 16:27:13 +0100

    binding avec service

--- src/app/recipes/components/recipe-list/recipe-list.component.ts
+++ src/app/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/core/models/recipe.model';
+import { recipesService } from 'src/app/core/services/recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  
+  recipes!:Recipe[];
+
+  constructor(private recipesService: recipesService) { }
+
+  ngOnInit(): void {
+    this.recipes= this.recipesService.recipes;
+  }
 
 }
