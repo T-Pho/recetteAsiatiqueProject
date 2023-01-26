15410457f3a8950b3100cdca47c8c79f0fb6b690
Tpho <thipho.doan@gmail.com>
2023-01-26 16:24:50 +0100

    fix pb de routing de la landing page

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { RecipeListComponent } from '../recipes/components/recipe-list/recipe-list.component';
 import { RecipesModule } from '../recipes/recipes.module';
-
-
+import { Router } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    LandingPageComponent
-  ],
-  imports: [
-    CommonModule,
-    RecipesModule
-  ],
-  exports:[
-    LandingPageComponent
-  ]
+  declarations: [LandingPageComponent],
+  imports: [CommonModule, RecipesModule],
+  exports: [LandingPageComponent],
 })
-export class LandingPageModule { }
+export class LandingPageModule implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onContinue(): void {
+    this.router.navigateByUrl('recipes');
+  }
+}
